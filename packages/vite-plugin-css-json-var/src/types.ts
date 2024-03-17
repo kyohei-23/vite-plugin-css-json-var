3e9c7eae98fd70c496f266086ae721a2b588bf55
KyoheiOrg <tgifz5jmr@gmail.com>
2024-03-17 22:50:45 +0900

    chore: add JSDoc comment

--- packages/vite-plugin-css-json-var/src/types.ts
+++ packages/vite-plugin-css-json-var/src/types.ts
@@ -1,6 +1,17 @@
 interface CommonOptions {
   /**
-   * path to configure file (format: json)
+   * path to setting json file or setting json object.  
+   * @property { JSON | string } file
+   * @example <caption>using path to setting file</caption>
+   * ```ts 
+   * file: "./site-config.json"
+   * ```
+   * @example <caption>passing setting file directly</caption>
+   * ```ts
+   * import setting_json from "./site-config.json"
+   * ~~~
+   * file: setting_json
+   * ```
    */
   file: JSON | string;
 }
@@ -9,16 +20,27 @@ interface CommonOptions {
 interface CssOptions extends CommonOptions {
   /**
    * Your using language for styling
+   * @property { "css" } lang
    */
   lang: "css";
   /**
-   * Output style for variable
-   */
+   * Output style for variable.\
+   *   if you are using css for styling, you can select only "css" option.
+   * @property { "css" } style
+  */
   style: "css";
 }
 
 interface PreprocessorOptions extends CommonOptions {
+  /**
+   * Your using language for styling.
+   * @property { "sass" | "scss" | "less" | "stylus" } lang
+  */
   lang: "scss" | "sass" | "less" | "stylus";
+  /**
+   * Output style for variable.
+   * @property { "css" | "preprocessor" } style
+  */
   style: "css" | "preprocessor";
 }
 
