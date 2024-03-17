ff4cc36b786b12e847632976ea959cafca6e9122
KyoheiOrg <tgifz5jmr@gmail.com>
2024-03-17 22:06:24 +0900

    chore: added test case

--- packages/vite-plugin-css-json-var/build.config.ts
+++ packages/vite-plugin-css-json-var/build.config.ts
@@ -3,4 +3,7 @@ export default {
   entries: ["./src/index"],
   declaration: "node16",
   clean: true,
+  replace: {
+    "import.meta.vitest": "undefined",
+  }
 } satisfies BuildConfig
--- packages/vite-plugin-css-json-var/src/core.ts
+++ packages/vite-plugin-css-json-var/src/core.ts
@@ -33,6 +33,17 @@ function getPrefix(type: Options["lang"]) {
   }
 }
 
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest
+  it("getConnector", () => {
+    expect(getPrefix("css")).toBe("--")
+    expect(getPrefix("sass")).toBe("$")
+    expect(getPrefix("scss")).toBe("$")
+    expect(getPrefix("less")).toBe("@")
+    expect(getPrefix("stylus")).toBe("")
+  })
+}
+
 function getConnector(type: Options["lang"]) {
   if (type === "stylus") {
     return "= "
@@ -40,6 +51,17 @@ function getConnector(type: Options["lang"]) {
   else {
     return ": "
   }
+}
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest
+  it("getConnector", () => {
+    expect(getConnector("css")).toBe(": ")
+    expect(getConnector("sass")).toBe(": ")
+    expect(getConnector("scss")).toBe(": ")
+    expect(getConnector("less")).toBe(": ")
+    expect(getConnector("stylus")).toBe("= ")
+  })
 }
 
 function getEOL(type: Options["lang"]) {
@@ -51,11 +73,44 @@ function getEOL(type: Options["lang"]) {
   }
 }
 
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest
+  it("getEOL", () => {
+    expect(getEOL("css")).toBe(";\n")
+    expect(getEOL("sass")).toBe("\n")
+    expect(getEOL("scss")).toBe(";\n")
+    expect(getEOL("less")).toBe(";\n")
+    expect(getEOL("stylus")).toBe("\n")
+  })
+}
+
+function getVariableGenerator(lang: Options["lang"]) {
+  const prefix = getPrefix(lang)
+  const connector = getConnector(lang)
+  const eol = getEOL(lang)
+
+  return function (key: string, value: string) {
+    return `${prefix}${key}${connector}${value}${eol}`
+  }
+
+}
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest
+  const key = "main-color",
+    value = "#f00000"
+  it("outputTest", () => {
+    expect(getVariableGenerator("css")(key, value)).toBe(`--${key}: ${value};\n`)
+    expect(getVariableGenerator("sass")(key, value)).toBe(`$${key}: ${value}\n`)
+    expect(getVariableGenerator("scss")(key, value)).toBe(`$${key}: ${value};\n`)
+    expect(getVariableGenerator("less")(key, value)).toBe(`@${key}: ${value};\n`)
+    expect(getVariableGenerator("stylus")(key, value)).toBe(`${key}= ${value}\n`)
+  })
+}
+
 export function convertObjToCssVar(obj: Record<string, string>, option: Options): string {
   const outputLang = option.style === "css" ? "css" : option.lang
-  const prefix = getPrefix(outputLang)
-  const connector = getConnector(outputLang)
-  const eol = getEOL(outputLang)
-  const data = Object.entries(obj).reduce((result, [key, value]) => result += `${prefix}${key}${connector}${value}${eol}`, "");
+  const variiableGenerator = getVariableGenerator(outputLang)
+  const data = Object.entries(obj).reduce((result, [key, value]) => result += variiableGenerator(key, value), "");
   return data
 }
